be6c3433409228a496a9d10e11bd7f68bde5b0fc
Guilherme Ferraz <guigacamposferraz@gmail.com>
2023-12-10 19:40:17 -0300

    Add responsivity and change number style

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 const date = document.getElementById('date');
 const day = document.getElementById('day');
 const month = document.getElementById('month');
-const year = document.getElementById('year');
+const innerYear = document.getElementById('inner-year');
 
-const today = new Date();
+const now = new Date();
 
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-date.innerHTML = today.getDate().toString().padStart(2, '0');
-day.innerHTML = weekDays[today.getDay()];
-month.innerHTML = months[today.getMonth()];
-console.log(months[today.getMonth()]);
-year.innerHTML = today.getFullYear();
+const today = now.getDate().toString().padStart(2, '0');
+date1.innerHTML = today.slice(0, 1);
+date2.innerHTML = today.slice(1, 2);
+day.innerHTML = weekDays[now.getDay()];
+month.innerHTML = months[now.getMonth()];
+innerYear.innerHTML = now.getFullYear().toString().slice(2, 4);
